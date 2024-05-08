4dd65367ccf92c3cc58c68dee6692412b446ca6c
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-05-08 21:31:30 +0200

    feat: add mdx support

---
+++ app/mdx-page/page.js
@@ -0,0 +1,13 @@
+"use client";
+import Welcome from "@/markdown/welcome.mdx";
+import MyComponent from "@/components/mycomponent";
+
+export default function Page() {
+  return (
+    <div>
+      <div>
+        <Welcome />
+      </div>
+    </div>
+  );
+}
---
+++ components/mycomponent.js
@@ -0,0 +1,7 @@
+export default function MyComponent() {
+  return (
+    <div>
+      <div>mycomponent</div>
+    </div>
+  );
+}
