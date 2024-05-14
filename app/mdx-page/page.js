cc50a95dcfa90e2f57332b0522fcd9a102f0c4ca
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-05-14 21:09:44 +0200

    feat: add centered slides

--- app/mdx-page/page.js
+++ app/mdx-page/page.js
@@ -1,12 +1,23 @@
 "use client";
 import Welcome from "@/markdown/welcome.mdx";
+import Welcome2 from "@/markdown/welcome2.mdx";
+import style from "./page.module.css";
+import { useMDXComponents } from "@mdx-js/react";
 
 export default function Page() {
+  const pages = [Welcome, Welcome2];
   return (
-    <div>
-      <div>
-        <Welcome />
-      </div>
+    <div className="flex flex-col">
+      {pages.map((page) => {
+        console.dir(page);
+        return (
+          <div className="w-full h-screen">
+            <div className={`${style.content} w-[1024px]`}>
+              {useMDXComponents(page)}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
