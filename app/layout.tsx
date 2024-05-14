75d5b5de626af34a6f9752adc65f7b22ec4d03c8
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-05-14 21:07:42 +0200

    feat: update font type to work sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Work_Sans({ weight: "300", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
