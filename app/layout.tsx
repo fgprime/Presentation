c6c74c30a329a2fea5b34ee7591545b4759629ac
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-05-15 23:06:38 +0200

    feat: add bold font variant

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Work_Sans({ weight: "300", subsets: ["latin"] });
+const inter = Work_Sans({ weight: ["200", "300"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Presentation",
