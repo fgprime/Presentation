d365ccc8a8045227ef5201671667728a2aee78c5
franzus85 <franz.straube@bmw.de>
2024-05-16 09:28:18 +0200

    propper var naming convention

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Work_Sans({ weight: ["200", "300"], subsets: ["latin"] });
+const workSans = Work_Sans({ weight: ["200", "300"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Presentation",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={workSans.className}>{children}</body>
     </html>
   );
 }
