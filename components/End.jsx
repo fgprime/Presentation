e71a25a560b7f9c31e4c7cbc48b3def29c1f8bef
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-05-20 17:57:30 +0200

    feat: add dark mode as default

---
+++ components/End.jsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div key="end">
+      <div className="mx-auto flex justify-center items-center min-h-screen w-[90vw] text-9xl font-thin">
+        <div className="transform origin-top rotate-45 ">
+          <div>+</div>
+        </div>
+      </div>
+    </div>
+  );
+}
