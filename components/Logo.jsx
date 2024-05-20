c935aaf2165481989661925187b0457ba24b03f3
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-05-20 17:56:57 +0200

    feat: change intro to logo component

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,11 +1,11 @@
-import Logo from "../public/vercel.svg";
+import LogoSource from "../public/vercel.svg";
 import Image from "next/image";
 
-export default function Intro() {
+export default function Logo() {
   return (
-    <div className=" absolute right-16 top-16 w-32">
+    <div className="absolute right-16 top-16 w-32">
       <Image
-        src={Logo}
+        src={LogoSource}
         style={{
           filter: "invert(100%) sepia(100%) saturate(100%) hue-rotate(180deg)",
         }}
